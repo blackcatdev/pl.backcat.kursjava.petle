d572f53d59b0dd95d558af757776a4686bb07ba1
student219285 <219285@student.pwr.edu.pl>
2019-09-11 15:27:17 +0200

    +VER2 - "repair"

--- src/pl/blackcat/kursjava/petle/temperature.java
+++ src/pl/blackcat/kursjava/petle/temperature.java
@@ -5,15 +5,19 @@ public class temperature {
     public static void main(String[] args) {
         int temperature = 35;
         //VER1: sprawdzanie switcha, czy zachowuje się podobnie do C++
+        //VER2: naprawa prostego pliku
         switch (temperature) {
             case 35 : {
                 System.out.println("Jesteś bardzo osłabiony!");
+                break;
             }
             case 36 : {
                 System.out.println("Jesteś niezbyt osłabiony!");
+                break;
             }
             case 37 : {
                 System.out.println("Masz stan podgorączkowy!");
+                break;
             }
             case 38 : {
                 System.out.println("Masz gorączkę!");
@@ -21,9 +25,11 @@ public class temperature {
             }
             case 39 : {
                 System.out.println("Masz wysoką gorączkę!");
+                break;
             }
             default : {
                 System.out.println("Temperatura poza skalą, natychmiast biegnij do lekarza lub kup nowy termometr. :>");
+                break;
             }
         }
 
